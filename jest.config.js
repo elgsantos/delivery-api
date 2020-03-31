cf9ee675aed6ec89912de0fa041ba71cdb41d999
Eduardo Gomes <elgsantos@gmail.com>
2020-03-31 02:33:59 -0300

    jest test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   // Stop running tests after `n` failures
   bail: true,
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: ['src/**'],
+  collectCoverageFrom: ['**/src/**/*.js'],
   // The directory where Jest should output its coverage files
-  // coverageDirectory: '__tests__/coverage',
+  coverageDirectory: 'coverage',
   // The test environment that will be used for testing
-  testEnvironment: 'node',
-  // The glob patterns Jest uses to detect test files
-  testMatch: [
-    '**/src/**/*.test.js?(x)'
-  ]
+  testEnvironment: 'node'
 };
